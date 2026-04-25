0b2f49af475b0cf21f74d3501362919d50666fdd
justin <justinmendonsa2004@gmail.com>
2026-04-25 21:34:04 +0530

    update code

---
+++ react-sample2/src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+import Body from "./Body";
+import cards from "./cards";
+import Cards from "./cards";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Body />
+    </>
+  );
+}
+
+export default App;
