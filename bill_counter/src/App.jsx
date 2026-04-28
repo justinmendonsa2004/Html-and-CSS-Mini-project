74e7aea54de9eb12aa5a70ae0d9279de7f427612
justin <justinmendonsa2004@gmail.com>
2026-04-28 14:11:41 +0530

    update code

--- bill_counter/src/App.jsx
+++ bill_counter/src/App.jsx
@@ -38,96 +38,29 @@ function App() {
 
   return (
     <>
-      <h1>Billing counter</h1>
-      <div className="customerdetails">
-        <div className="customername">
-          <label htmlFor="name">Customer Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={customerName}
-            onChange={(e) => setcustomerName(e.target.value)}
-          />
-        </div>
-        <div className="customerPhone">
-          <label htmlFor="contact">Customer Phone:</label>
-          <input
-            type="phone"
-            name="contact"
-            value={customerPhone}
-            onChange={(e) => setcustomerPhone(e.target.value)}
-          />
-        </div>
-      </div>
-      {productList && (
-        <div className="allProducts">
-          <table>
-            <tr>
-              <th>SL</th>
-              <th>Name</th>
-              <th>QTY</th>
-              <th>Total</th>
-            </tr>
-            {productList.map((element, index) => (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{element.ProductName}</td>
-                <td>{element.QTY}</td>
-                <td>{element.Total}</td>
-              </tr>
-            ))}
-          </table>
-          <button onClick={handleGeneretBills}>Generate Bill</button>
-        </div>
-      )}
-      <div className="itemInput">
-        <div className="productNamebox">
-          <label htmlFor="productname">Product Name:</label>
-          <input
-            type="text"
-            name="productname"
-            value={ProductName}
-            onChange={(e) => setProductName(e.target.value)}
-          />
-        </div>
-        <div className="productQtyBox">
-          <label htmlFor="productQTY">QTY:</label>
-          <input
-            type="number"
-            name="productQTY"
-            value={ProductQTY}
-            onChange={(e) => setProductQTY(e.target.value)}
-          />
-        </div>
-        <div className="productQtyBox">
-          <label htmlFor="productprice">Price:</label>
-          <input
-            type="number"
-            name="productprice"
-            value={ProductPrice}
-            onChange={(e) => setProductPrice(e.target.value)}
-          />
-        </div>
-        <div className="TotalAmount">
-          <p>
-            <b>Total:</b>
-            Rs.{ProductPrice * ProductQTY}
-          </p>
-        </div>
-        <div className="AddItemBox">
-          <button onClick={handleAddButton}>Add Item</button>
-        </div>
-      </div>
-      {showGeneratedBill && (
-        <div className="generatedBillBox">
-          <h1>Generated Bills</h1>
-          <div className="customerBox">
-            <p>
-              <b>Customer Name:</b>
-              {customerName} <b>Contact:</b>
-              {customerPhone}
-            </p>
+      <div className="Billcalculate">
+        <h1>Billing Counter</h1>
+        <div className="customerdetails">
+          <div className="customername">
+            <label htmlFor="name">Customer Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={customerName}
+              onChange={(e) => setcustomerName(e.target.value)}
+            />
           </div>
+          <div className="customerPhone">
+            <label htmlFor="contact">Customer Phone:</label>
+            <input
+              type="phone"
+              name="contact"
+              value={customerPhone}
+              onChange={(e) => setcustomerPhone(e.target.value)}
+            />
+          </div>
+        </div>
+        {productList.length > 0 && (
           <div className="allProducts">
             <table>
               <tr>
@@ -145,10 +78,83 @@ function App() {
                 </tr>
               ))}
             </table>
-            <button onClick={ResetBills}>Reset</button>
+            <button id="Button" onClick={handleGeneretBills}>
+              Generate Bill
+            </button>
+          </div>
+        )}
+        <div className="itemInput">
+          <div className="productNamebox">
+            <label htmlFor="productname">Product Name:</label>
+            <input
+              type="text"
+              name="productname"
+              value={ProductName}
+              onChange={(e) => setProductName(e.target.value)}
+            />
+          </div>
+          <div className="productQtyBox">
+            <label htmlFor="productQTY">QTY:</label>
+            <input
+              type="number"
+              name="productQTY"
+              value={ProductQTY}
+              onChange={(e) => setProductQTY(e.target.value)}
+            />
+          </div>
+          <div className="productQtyBox">
+            <label htmlFor="productprice">Price:</label>
+            <input
+              type="number"
+              name="productprice"
+              value={ProductPrice}
+              onChange={(e) => setProductPrice(e.target.value)}
+            />
+          </div>
+          <div className="TotalAmount">
+            <p>
+              <b>Total:</b>
+              Rs.{ProductPrice * ProductQTY}
+            </p>
+          </div>
+          <div className="AddItemBox">
+            <button onClick={handleAddButton}>Add Item</button>
           </div>
         </div>
-      )}
+        {showGeneratedBill && (
+          <div className="generatedBillBox">
+            <h1>Generated Bills</h1>
+            <div className="customerBox">
+              <p>
+                <b>Customer Name:</b>
+                {customerName} <b>Contact:</b>
+                {customerPhone}
+              </p>
+            </div>
+            <div className="allProducts">
+              <table>
+                <tr>
+                  <th>SL</th>
+                  <th>Name</th>
+                  <th>QTY</th>
+                  <th>Total</th>
+                </tr>
+                {productList.map((element, index) => (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{element.ProductName}</td>
+                    <td>{element.QTY}</td>
+                    <td>{element.Total}</td>
+                  </tr>
+                ))}
+              </table>
+              <button id="Button" onClick={ResetBills}>
+                Reset
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
