cd2d05f0f8d9bb6e04cbd2b2b7942011ed52cefe
justin <justinmendonsa2004@gmail.com>
2026-04-28 12:42:46 +0530

    update code

--- bill_counter/src/App.jsx
+++ bill_counter/src/App.jsx
@@ -6,6 +6,35 @@ function App() {
   const [ProductName, setProductName] = useState("");
   const [ProductQTY, setProductQTY] = useState(0);
   const [ProductPrice, setProductPrice] = useState(0);
+  const [productList, setProductList] = useState([]);
+  const [showGeneratedBill, setShowGeneratedBills] = useState(false);
+
+  const handleAddButton = () => {
+    let obj1 = {
+      ProductName: ProductName,
+      QTY: ProductQTY,
+      Total: ProductQTY * ProductPrice,
+    };
+    setProductList([...productList, obj1]);
+    setProductName("");
+    setProductQTY(0);
+    setProductPrice(0);
+  };
+
+  const handleGeneretBills = () => {
+    if (!showGeneratedBill) {
+      setShowGeneratedBills(true);
+    }
+  };
+
+  const ResetBills = () => {
+    setProductList([]);
+    setcustomerName("");
+    setcustomerPhone("+91");
+    setProductName("");
+    setProductQTY(0);
+    setProductPrice(0);
+  };
 
   return (
     <>
@@ -30,17 +59,27 @@ function App() {
           />
         </div>
       </div>
-      <div className="allProducts">
-        <table>
-          <tr>
-            <th>SL</th>
-            <th>Name</th>
-            <th>QTY</th>
-            <th>Total</th>
-            <th>RM</th>
-          </tr>
-        </table>
-      </div>
+      {productList && (
+        <div className="allProducts">
+          <table>
+            <tr>
+              <th>SL</th>
+              <th>Name</th>
+              <th>QTY</th>
+              <th>Total</th>
+            </tr>
+            {productList.map((element, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{element.ProductName}</td>
+                <td>{element.QTY}</td>
+                <td>{element.Total}</td>
+              </tr>
+            ))}
+          </table>
+          <button onClick={handleGeneretBills}>Generate Bill</button>
+        </div>
+      )}
       <div className="itemInput">
         <div className="productNamebox">
           <label htmlFor="productname">Product Name:</label>
@@ -76,9 +115,40 @@ function App() {
           </p>
         </div>
         <div className="AddItemBox">
-          <button>Add Item</button>
+          <button onClick={handleAddButton}>Add Item</button>
         </div>
       </div>
+      {showGeneratedBill && (
+        <div className="generatedBillBox">
+          <h1>Generated Bills</h1>
+          <div className="customerBox">
+            <p>
+              <b>Customer Name:</b>
+              {customerName} <b>Contact:</b>
+              {customerPhone}
+            </p>
+          </div>
+          <div className="allProducts">
+            <table>
+              <tr>
+                <th>SL</th>
+                <th>Name</th>
+                <th>QTY</th>
+                <th>Total</th>
+              </tr>
+              {productList.map((element, index) => (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{element.ProductName}</td>
+                  <td>{element.QTY}</td>
+                  <td>{element.Total}</td>
+                </tr>
+              ))}
+            </table>
+            <button onClick={ResetBills}>Reset</button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
